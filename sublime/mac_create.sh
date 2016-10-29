f19f64c51c317f87c310303b0d22e770fcbd3812
Zoltan Feher <lostface@fastmail.fm>
2016-10-29 13:00:08 +0200

    Add sublime create scripts

---
+++ sublime/mac_create.sh
@@ -0,0 +1,4 @@
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+mkdir ~/dotfiles/sublime
+mv User ~/dotfiles/sublime/
+ln -s ~/dotfiles/sublime/User
---
+++ sublime/win_create.bat
@@ -0,0 +1,4 @@
+cd "%APPDATA%\Sublime Text 3\Packages\"
+mkdir c:\dev\dotfiles\sublime
+move User c:\dev\dotfiles\sublime\
+mklink /D User c:\dev\dotfiles\sublime\User
