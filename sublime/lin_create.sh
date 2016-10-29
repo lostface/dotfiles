5685a222f4a8faf49673c88387018ee4ec8a80d7
Zoltan Feher <lostface@fastmail.fm>
2016-10-29 13:01:56 +0200

    Add linux sublime scripts

---
+++ sublime/lin_create.sh
@@ -0,0 +1,4 @@
+cd ~/.config/sublime-text-3/Packages/
+mkdir ~/dotfiles/sublime
+mv User ~/dotfiles/sublime/
+ln -s ~/dotfiles/sublime/User
---
+++ sublime/lin_restore.sh
@@ -0,0 +1,3 @@
+cd ~/.config/sublime-text-3/Packages/
+rm -r User
+ln -s ~/dotfiles/sublime/User
